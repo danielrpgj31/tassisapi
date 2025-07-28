cc626623b848e65ffffb112d38ef8f58980ab62a
Daniel Ribeiro <daniel.junior@gmail.com>
2025-07-28 09:37:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### Referências
+
+- [Como usar webhooks na API do WhatsApp Business](https://business.whatsapp.com/blog/how-to-use-webhooks-from-whatsapp-business-api)
+- [Facebook for Developers - Apps](https://developers.facebook.com/apps/)
+- [Seu Authtoken no Ngrok](https://dashboard.ngrok.com/get-started/your-authtoken)
+- [Gerenciador de Números do WhatsApp Business](https://business.facebook.com/latest/whatsapp_manager/phone_numbers)
+
+### Comandos
+
+#### Teste de chamada do webhook
+
+```bash
+curl "https://ec2-54-86-175-192.compute-1.amazonaws.com:3000/webhook?hub.mode=subscribe"
+
+curl "http://localhost:3000/webhook?hub.mode=subscribe&hub.verify_token=seu_token_secreto"
+
+curl "https://8c70434db9c5.ngrok-free.app/webhook?hub.mode=subscribe&hub.verify_token=seu_token_secreto"
+```
+
+#### Transferência de arquivos de projeto
+
+```bash
+scp -i tassis-aws-key.pem ./whatsapp-webhook/index.js ubuntu@ec2-54-86-175-192.compute-1.amazonaws.com:/home/ubuntu/whatsapp-webhook
+```
---
+++ whatsapp-webhook/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Token que você define no painel do WhatsApp Meta
+const VERIFY_TOKEN = 'seu_token_secreto';
+
+app.use(bodyParser.json());
+
+// Verificação do webhook (GET)
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  console.log('Verificando webhook:', { mode, token, challenge });
+  console.log('Mode:', mode);
+  console.log('Token:', token);
+  
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// Recebimento de mensagens (POST)
+app.post('/webhook', (req, res) => {
+  console.log('Mensagem recebida:', JSON.stringify(req.body, null, 2));
+  res.sendStatus(200); // Meta exige 200 OK
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
